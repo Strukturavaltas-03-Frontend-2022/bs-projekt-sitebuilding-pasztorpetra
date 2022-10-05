271268249e871ff0b150dee7cdb6c6bbd13a352b
Petra <pasztor.petra1998@gmail.com>
2022-10-05 14:44:24 +0200

    csinosítás

--- main.js
+++ main.js
@@ -1,2 +1,22 @@
+"use strict"
 
-const navbar = document.querySelector('.navbar');
+// const navbar = document.querySelector('.navbar');
+// const navBrand = document.querySelector(".navbar-brand");
+// const navItem = document.querySelectorAll(".nav-item");
+// const navToggler = document.querySelector(".navbar-toggler");
+
+// const changeNavbar = () => {
+//   if (window.scrollY !== 0) {
+//     navbar.style.removeProperty('background-image');
+//     navbar.classList.add("bg-white");
+//     navBrand.style.color = "black";
+//     navToggler.style.color = "black";
+//   } else {
+    
+//   };
+// };
+
+// window.addEventListener("scroll", changeNavbar);
+
+
+
